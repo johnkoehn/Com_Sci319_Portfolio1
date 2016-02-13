a4900b10bdb507e680e397a55f4616fbe20a3d3b
John Koehn <kingkoehn@gmail.com>
2016-02-13 16:43:42 -0600

    Added User Class

---
+++ src/core/User.java
@@ -0,0 +1,47 @@
+package core;
+
+public class User
+{
+	private int points;
+	private String username;
+	private String password;
+	
+	public User(int points, String username, String password)
+	{
+		this.points = points;
+		this.username = username;
+		this.password = password;
+		
+	}
+	
+	public void addPoints(int points)
+	{
+		this.points += points;
+	}
+
+	public boolean subtractPoints(int points)
+	{
+		if((this.points - points) < 0)
+		{
+			return false;
+		}
+		
+		this.points -= points;
+		return true;
+	}
+	
+	public String printUserInfo()
+	{
+		return username + "," + password + "," + Integer.toString(points);
+	}
+	
+	public String getPassowrd()
+	{
+		return password;
+	}
+	
+	public String getUsername()
+	{
+		return username;
+	}
+}
