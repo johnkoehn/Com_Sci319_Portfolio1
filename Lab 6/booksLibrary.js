a585225e000b2ef083f1b2a4bcc720339bd538da
Brandon Huegli <bjhuegli@iastate.edu>
2016-03-04 01:57:49 +0000

    fuck you buddy

--- Lab 6/booksLibrary.js
+++ Lab 6/booksLibrary.js
@@ -1,8 +1,8 @@
 function Library()
 {
-	var books1 = [new Book("book1", false), new Book("book1", true), new Book("book2", true), new Book("book3", true), new Book("book4", false), new Book("book4", true), new Book("book4", true), new Book("book5", false), new Book("book1", true)];
-	var books2 = [new Book("book1", false), new Book("book1", true), new Book("book2", true), new Book("book3", true), new Book("book3", true), new Book("book4", false), new Book("book4", true), new Book("book4", true), new Book("book5", false), new Book("book1", true)];
-	var books3 = [new Book("book1", false), new Book("book1", true), new Book("book2", true), new Book("book3", true), new Book("book3", true), new Book("book4", false), new Book("book4", true), new Book("book4", true), new Book("book5", false), new Book("book1", true)];
+	var books1 = [new Book("book5", false), new Book("book70", true), new Book("book23", true), new Book("book9", false), new Book("book1", true)];
+	var books2 = [new Book("book9", false), new Book("book72", true), new Book("book3", true), new Book("book80", true), new Book("book12", true), new Book("book45", false), new Book("book21", true)];
+	var books3 = [new Book("book3", false), new Book("book3", true), new Book("book42", true), new Book("book1", true), new Book("book21", false), new Book("book6", true), new Book("book7", true), new Book("book2", false), new Book("book4", true)];
 	this.shelfs = [new Shelf("Shelf1", books1, 10), new Shelf("Shelf2", books2, 10), new Shelf("Shelf3", books3, 10)];
 	
 }
@@ -34,14 +34,13 @@ Library.prototype.display = function()
 	var books = 0;
 	var s;
 	
-	
 	s = "<table id=\"myTable\" border=3 width = 300px height = 500px>"
 	s += "<tr>";
 	for(var i = 0; i < shelfs.length; ++i)
 	{
 		if(shelfs[i].size > books)
 			books = shelfs[i].size;
-		s += "<td>" + shelfs[i].shelfName + "</td>";
+		s += "<td bgcolor = \"#56c61d\" align = \"center\">" + shelfs[i].shelfName + "</td>";
 	}
 	s+= "</tr>";
 	for(i = 0; i < books; ++i)
@@ -52,7 +51,7 @@ Library.prototype.display = function()
 			if(i >= shelfs[j].books.length)
 				s += "<td></td>";
 			else
-				s += "<td id = \"book\">" + shelfs[j].books[i].bookName + "</td>";
+				s += "<td id = \"book\" bgcolor = \"ecf475\" align = \"center\">" + shelfs[j].books[i].bookName + "</td>";
 		}
 		s += "</tr>";
 	}
